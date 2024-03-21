f9eb13025211dcc3df4857f28444f67530bc00fe
psmounika-16 <150592895+psmounika-16@users.noreply.github.com>
2024-03-21 11:45:56 +0530

    Add files via upload

---
+++ leetcode_1669.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        res=list1
+        list2_tmp=list2
+        count=0
+        while count<a-1:
+            list1=list1.next
+            count+=1
+        list1_tmp=list1.next
+        list1.next=list2
+        while count<b:
+            list1_tmp=list1_tmp.next
+            count+=1
+        while list2_tmp.next:
+            list2_tmp=list2_tmp.next
+        list2_tmp.next=list1_tmp
+        return res

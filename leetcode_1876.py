d527c1d734f5484dc55cb43d4793c19d5ae667ee
psmounika-16 <150592895+psmounika-16@users.noreply.github.com>
2024-04-02 14:23:30 +0530

    Add files via upload

---
+++ leetcode_1876.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        windowsize=3
+        slidingwindowcount=len(s)-windowsize+1
+        index=0
+        count=0
+        while slidingwindowcount>0:
+            lst=[]
+            temstr=s[index:windowsize+index]
+            for i in temstr:
+                if i in lst:
+                    break
+                else:
+                    lst.append(i)
+            else:
+                count+=1
+            index+=1
+            slidingwindowcount-=1
+        return count

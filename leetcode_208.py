a4202f6c3cd4ecc938953c0f67a8bd925acc9019
psmounika-16 <150592895+psmounika-16@users.noreply.github.com>
2024-04-26 22:56:53 -0700

    Add files via upload

---
+++ leetcode_208.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.isWord=False
+class Trie:
+    def __init__(self):
+        self.root=TrieNode()
+    def insert(self, word: str) -> None:
+        node:TrieNode=self.root
+        for c in word:
+            node=node.children.setdefault(c,TrieNode())
+        node.isWord=True
+    def search(self, word: str) -> bool:
+        node:TrieNode =self._find(word)
+        return node and node.isWord
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._find(prefix)
+    def _find(self,prefix:str)->Optional[TrieNode]:
+        node:TrieNode=self.root
+        for c in prefix:
+            if c not in node.children:
+                return None
+            node=node.children[c]
+        return node
+
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
